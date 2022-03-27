86e115ae002719331e14120bf38dafb9ccfda8dc
seonja <seonja.kim0@gmail.com>
2022-03-27 21:26:55 +0900

    [fix] input autocomplete off

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ const cache = new Map()
 class App extends Component {
   template() {
     return `
-    <div class='auto-complete-input'>
+    <div class="auto-complete-input">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" class="css-1emtzzi"><path fill="currentColor" fill-rule="evenodd" d="M16.173 16.43a7.5 7.5 0 11.257-.257.749.749 0 00-.257.257zm.639 1.442a9 9 0 111.06-1.06l3.88 3.88a.75.75 0 11-1.06 1.06l-3.88-3.88z" clip-rule="evenodd"></path></svg>
-      <input type='text' id='auto-complete'/>
-      <button id='clear-button'>
+      <input type="text" id="auto-complete" autocomplete="off"/>
+      <button id="clear-button">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" class="css-1orrb02-IcClear"><path fill="currentColor" fill-rule="evenodd" d="M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11zm-8.12-3.96a.764.764 0 011.08 1.08L13.08 12l2.88 2.88a.764.764 0 01-1.08 1.08L12 13.08l-2.88 2.88a.764.764 0 01-1.08-1.08L10.92 12 8.04 9.12a.764.764 0 011.08-1.08L12 10.92l2.88-2.88z" clip-rule="evenodd"></path></svg>
       </button>
     </div>
